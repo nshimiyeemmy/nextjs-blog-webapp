1280c279ef401eee3ebbbb7a90ea7fdfef3fa7e3
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-11-17 15:05:58 +0200

    updated with new entries

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,23 @@
-import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap'
 
+import { AuthorIntro } from 'components/AuthorIntro';
+import { CardItem } from 'components/CardItem';
+import { CardListItem } from 'components/CardListItem';
+import PageLayout from 'components/PageLayout';
+import { Col, Row } from 'react-bootstrap';
 const index = () => {
   return (
-    <div>
-      <Navbar defaultExpanded bg="light" expand="lg">
-  <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
-      </Nav>
-      <Form className="d-flex"  style={{paddingLeft:"45rem"}}>
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-      <h1>Hello world</h1>
-    </div>
+    <PageLayout>
+    <AuthorIntro />
+    <hr/>
+    <Row className="mb-5">
+        <Col md="10">
+          <CardListItem />
+        </Col>
+        <Col md="4">
+          <CardItem />
+        </Col>
+      </Row>
+</PageLayout>
   )
 }
 
