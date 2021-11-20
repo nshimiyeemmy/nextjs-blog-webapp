b61ad41e9496782f4cd694fce674cdcb9972bcd8
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-11-20 20:00:47 +0200

    updated with the defualt values for author name and avatar image

--- components/CardItem.js
+++ components/CardItem.js
@@ -11,13 +11,13 @@ export const CardItem = ({title,subtitle,date,image,author}) => {
                 className="d-flex flex-row">
                 <img
                   // src={'https://via.placeholder.com/150'}
-                  src={author?.avatar}
+                  src={author?.avatar || 'https://via.placeholder.com/150'}
                   className="rounded-circle mr-3"
                   height="50px"
                   width="50px"
                   alt="avatar"/>
                 <div>
-                  <Card.Title className="font-weight-bold mb-1">{author?.username}</Card.Title>
+                  <Card.Title className="font-weight-bold mb-1">{author?.username || 'Guest'}</Card.Title>
                   <Card.Text className="card-date">{date}</Card.Text>
                 </div>
               </Card.Header>
